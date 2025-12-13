0cd40636890a07e9b1822ef204c7934198c9f452
wesam-jop <wesam-jop@hotmail.com>
2025-12-13 13:26:39 +0300

    add tawk chat

--- web/resources/views/app.blade.php
+++ web/resources/views/app.blade.php
@@ -37,7 +37,17 @@
     </head>
     <body class="font-sans antialiased {{ $isRTL ? 'font-cairo' : '' }}" dir="{{ $isRTL ? 'rtl' : 'ltr' }}">
         @inertia
-        
+        <script type="text/javascript">
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/693d3b794f7afe19760b372d/1jcbj0lih';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+        </script>
         <script>
             (function() {
                 const SUPPORTED_LOCALES = ['ar', 'en'];
